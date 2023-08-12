94de87d3cf659036878b4ce01ed7cfbe29cdfb57
ricky-kiva <ricky.kiva@gmail.com>
2023-08-12 15:33:48 +0700

    build(gradle): add dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,4 +44,13 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.9.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
+    implementation("androidx.activity:activity-ktx:1.6.1") //by viewModels()
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1") //lifecycleScope
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.5.1") //asLiveData
 }
